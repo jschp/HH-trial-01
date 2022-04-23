eed58074dd964f7ff0c192eeebad4ca34afef675
Tony Sun <tonysun@M1-of-the-Sun.local>
2022-04-23 23:45:45 +0200

    pushed to kovan and mainnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,16 +6,16 @@ require("dotenv").config();
  */
  module.exports = {
   networks: {
-    // for mainnet
-    // 'optimism': {
-    //   url: "https://mainnet.optimism.io",
-    //   accounts: [privateKey1, privateKey2]
-    // },
-    // for testnet
-    'optimism-kovan': {
-      url: "https://kovan.optimism.io",
-      accounts: [process.env.KOVAN_PRIV_KEY]
+    // for mainnet. Deployed contract: 0x8A366410E896fD5E46ADD02Ac5758682021D4124
+    'optimism': {
+      url: "https://mainnet.optimism.io",
+      accounts: [process.env.PRIVATE_KEY]
     },
+    // for testnet. Deployed contract: 0xf4f25078f0Ab1E6BEeAA7f074F0a6F93b91972cA
+    // 'optimism-kovan': {
+    //   url: "https://kovan.optimism.io",
+    //   accounts: [process.env.PRIVATE_KEY]
+    // },
     // for the local dev environment
     // 'optimism-local': {
     //   url: "http://localhost:8545",
